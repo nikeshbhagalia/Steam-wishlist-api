0aebfd607e9f1a7d1b88f6a3861e03ea964271df
Nikesh Bhagalia <nbha675@aucklanduni.ac.nz>
2020-05-17 16:53:01 +1200

    Init

---
+++ Steam wishlist api/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Steam_wishlist_api.Models
+{
+    public class Game
+    {
+        public string Added { get; set; }
+
+        public string Name { get; set; }
+
+        public string Capsule { get; set; }
+    }
+}
